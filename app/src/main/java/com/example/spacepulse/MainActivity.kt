6cd6ed9e041bbd8828963047b00fcecf6b1748c8
Gabriel <u202321281@upc.edu.pe>
2026-05-14 10:27:18 -0500

    Primer commit

---
+++ app/src/main/java/com/example/spacepulse/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.spacepulse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.spacepulse.ui.theme.SpacePulseTheme
+
+import com.example.taskmaster.views.pages.FirstPage
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SpacePulseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        FirstPage()
+
+                    }
+                }
+            }
+        }
+    }
+}
